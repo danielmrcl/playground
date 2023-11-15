b29b91c118fbb299cbba40524c50efc84b0fa498
Daniel Jr. <daniels.ms@pm.me>
2023-11-15 20:48:55 -0300

    Update angular-first-app first component.

--- angular-first-app/src/app/app.component.ts
+++ angular-first-app/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-first-app';
+  showMessages = true;
+  message = 'Hello world!!!!';
+
+  toggleMessages() {
+    this.showMessages = !this.showMessages;
+  }
 }
