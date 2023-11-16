4a794dafc2ec440c3e9b858460da7c5ce4a577af
Daniel Jr. <daniels.ms@pm.me>
2023-11-16 00:57:34 -0300

    Change angular modules to standalone components.

--- angular-first-app/src/app/app.component.ts
+++ angular-first-app/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [
+    HomeComponent
+  ]
 })
 export class AppComponent {
   title = 'homes';
