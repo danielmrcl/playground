c9230f8483ce0ead5baa5ee559c01d2fbef94cb3
Daniel Jr. <daniels.ms@pm.me>
2023-11-20 17:45:38 -0300

    Add housing location interface.

--- angular-first-app/src/app/home/home.component.ts
+++ angular-first-app/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   standalone: true,
@@ -11,5 +12,16 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
   ]
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
