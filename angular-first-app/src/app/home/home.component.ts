4e51de629392ce4e2a7f52dcbe30e0bea22058c8
Daniel Jr. <daniels.ms@pm.me>
2023-11-20 16:00:06 -0300

    Add housing location component.

--- angular-first-app/src/app/home/home.component.ts
+++ angular-first-app/src/app/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
   standalone: true,
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  imports: [
+    HousingLocationComponent
+  ]
 })
 export class HomeComponent {
 
---
+++ angular-first-app/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-housing-location',
+  templateUrl: './housing-location.component.html',
+  styleUrls: ['./housing-location.component.scss']
+})
+export class HousingLocationComponent {
+
+}
