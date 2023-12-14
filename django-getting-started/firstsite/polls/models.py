1051ce97b19d22ab03af7bc44239948a55b54b65
Daniel Jr. <daniels.ms@pm.me>
2023-12-14 02:37:17 -0300

    Add polls models and migration

--- django-getting-started/firstsite/polls/models.py
+++ django-getting-started/firstsite/polls/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+class Choice(models.Model):
+    question = models.ForeignKey(to=Question, on_delete=models.SET_NULL, null=True)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
