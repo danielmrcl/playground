5fcc0790698302bc54f57e2b4f98357e2df6b9c2
Daniel Jr. <daniels.ms@pm.me>
2023-12-16 01:52:58 -0300

    Add Question routes and templates.

--- django-getting-started/firstsite/polls/urls.py
+++ django-getting-started/firstsite/polls/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index),
+    path('<int:question_id>/', views.details),
+    path('<int:question_id>/results/', views.results),
+    path('<int:question_id>/votes/', views.votes)
 ]
--- django-getting-started/firstsite/polls/views.py
+++ django-getting-started/firstsite/polls/views.py
@@ -1,7 +1,18 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from .models import Question
+from django.http import HttpRequest, HttpResponse, Http404
+from django.shortcuts import get_object_or_404, render
 
 def index(request: HttpRequest):
-    print(request.headers)
-    return HttpResponse("Index")
+    questions = Question.objects.all()
+    return render(request, "polls/index.html", { "questions": questions })
+
+def details(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    return render(request, "polls/details.html", { "question": question })
+
+def results(request, question_id):
+    return HttpResponse("There are the results for question %s." % question_id)
+
+def votes(request, question_id):
+    return HttpResponse("This question %s have received _ votes." % question_id)
 
