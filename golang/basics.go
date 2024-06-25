5b0a3010f1534806b829b9bfc38d7c6204b4fcec
Daniel Jr <dan@danielmrcl.dev>
2024-06-25 01:16:13 -0300

    golang add struct and methods
    Signed-off-by: Daniel Jr <dan@danielmrcl.dev>

--- golang/basics.go
+++ golang/basics.go
@@ -200,3 +200,36 @@ func strings_examples() {
 		fmt.Println("found á!")
 	}
 }
+
+/**
+ * Structs: typed collections of fields.
+ * Receiver function: method that belongs to a single non-variadic type.
+ */
+type person struct {
+	name string
+	age uint8
+}
+func (p person) greetig() {
+	fmt.Printf("My name is %s and I have %d years old.\n", p.name, p.age)
+}
+func (p *person) incrementAge() {
+	p.age++
+}
+func structs_examples() {
+	p1 := person{name: "João", age: 54}
+	p2 := &p1
+	p2.age = 21
+	fmt.Println("person1:", p1)
+
+	p1.incrementAge()
+	p1.greetig()
+
+	p3 := struct {
+		name string
+		hairColor string
+	} {
+		"Pedro",
+		"Azul",
+	}
+	fmt.Println("person3:", p3)
+}
