2dfe23a97b35dca2327fd0040332ae8828808452
Daniel Jr <dan@danielmrcl.dev>
2024-07-07 02:04:27 -0300

    golang add module
    Signed-off-by: Daniel Jr <dan@danielmrcl.dev>

---
+++ golang/go.mod
@@ -0,0 +1,3 @@
+module my_examples
+
+go 1.22
---
+++ golang/main.go
@@ -0,0 +1,28 @@
+// Go By Examples
+package main
+
+import (
+	e "my_examples/examples"
+)
+
+func main() {
+	e.ValuesExamples()
+	e.InterfacesExamples()
+	e.StructsExamples()
+	e.StructEmbeddingExample()
+	e.MapsExamples()
+	e.LoopsExamples()
+	e.RangeExamples()
+	e.ArraysExamples()
+	e.SlicesExamples()
+	e.SwitchExamples()
+	e.StringsExamples()
+	e.PointersExamples()
+	e.ConstantsExamples()
+	e.FunctionsExamples()
+	e.VariablesExamples()
+	e.ConditionsExamples()
+	e.EnumsExamples()
+	e.GenericsExamples()
+}
+
