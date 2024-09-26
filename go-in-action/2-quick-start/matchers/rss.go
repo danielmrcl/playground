8e25cbf39b6706d3ae860653928373b26b35d168
Daniel Jr <dan@danielmrcl.dev>
2024-09-26 01:56:14 -0300

    (golang) add rss matcher
    Signed-off-by: Daniel Jr <dan@danielmrcl.dev>

--- go-in-action/2-quick-start/matchers/rss.go
+++ go-in-action/2-quick-start/matchers/rss.go
@@ -1,1 +1,126 @@
 package matchers
+
+import (
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+
+	"danielmrcl.dev/go-in-action/search"
+)
+
+type (
+	// item defines the fields associated with the item tag
+	// in the rss document.
+	item struct {
+		XMLName     xml.Name `xml:"item"`
+		PubDate     string   `xml:"pubDate"`
+		Title       string   `xml:"title"`
+		Description string   `xml:"description"`
+		Link        string   `xml:"link"`
+		GUID        string   `xml:"guid"`
+		GeoRssPoint string   `xml:"georss:point"`
+	}
+	
+	// image defines the fields associated with the image tag
+	// in the rss document.
+	image struct {
+		XMLName xml.Name `xml:"image"`
+		URL     string   `xml:"url"`
+		Title   string   `xml:"title"`
+		Link    string   `xml:"link"`
+	}
+	
+	// channel defines the fields associated with the channel tag
+	// in the rss document.
+	channel struct {
+		XMLName        xml.Name `xml:"channel"`
+		Title          string   `xml:"title"`
+		Description    string   `xml:"description"`
+		Link           string   `xml:"link"`
+		PubDate        string   `xml:"pubDate"`
+		LastBuildDate  string   `xml:"lastBuildDate"`
+		TTL            string   `xml:"ttl"`
+		Language       string   `xml:"language"`
+		ManagingEditor string   `xml:"managingEditor"`
+		WebMaster      string   `xml:"webMaster"`
+		Image          image    `xml:"image"`
+		Item           []item   `xml:"item"`
+	}
+
+	// rssDocument defines the fields associated with the rss document.
+	rssDocument struct {
+		XMLName xml.Name `xml:"rss"`
+		Channel channel  `xml:"channel"`
+	}
+)
+
+type rssMatcher struct{}
+
+func init() {
+	var matcher rssMatcher
+	search.Register("rss", matcher)
+}
+
+func (m rssMatcher) retrieve(feed *search.Feed) (*rssDocument, error) {
+	if feed.URI == "" {
+		return nil, errors.New("No rss feed URI provided")
+	}
+
+	response, err := http.Get(feed.URI)
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		return nil, fmt.Errorf("HTTP response error %d\n", response.StatusCode)
+	}
+
+	var document rssDocument
+	err = xml.NewDecoder(response.Body).Decode(&document)
+	return &document, err
+}
+
+func (m rssMatcher) Search(feed *search.Feed, searchTerm string) ([]*search.Result, error) {
+	var results []*search.Result
+
+	log.Printf("Search Feed Type[%s] Site[%s] for URI[%s]\n", feed.Type, feed.Name, feed.URI)
+
+	document, err := m.retrieve(feed)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, item := range document.Channel.Item {
+		matched, err := regexp.MatchString(searchTerm, item.Title)
+		if err != nil {
+			return nil, err
+		}
+
+		if matched {
+			results = append(results, &search.Result{
+				Field: "Title",
+				Content: item.Title,
+			})
+		}
+
+		matched, err = regexp.MatchString(searchTerm, item.Description)
+		if err != nil {
+			return nil, err
+		}
+
+		if matched {
+			results = append(results, &search.Result{
+				Field: "Description",
+				Content: item.Description,
+			})
+		}
+	}
+
+	return results, nil
+}
+
